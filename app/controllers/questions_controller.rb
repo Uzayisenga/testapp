b08fe63c4056bc42053fc3f9d55206da68f550da
Uzayisenga <aminauzayisenga@gmail.com>
2019-12-01 07:02:33 +0200

    make it better

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -24,11 +24,13 @@ class QuestionsController < ApplicationController
   # POST /questions
   # POST /questions.json
   def create
+    flash[:notice] = "Save the world by Giving life to helpless"
+    redirect_to @show
     @question = Question.new(question_params)
 
     respond_to do |format|
       if @question.save
-        format.html { redirect_to @question, notice: 'Question was successfully created.' }
+        format.html { redirect_to @question, notice: 'Thank you for asking!!! the doctors will help.' }
         format.json { render :show, status: :created, location: @question }
       else
         format.html { render :new }
--- app/models/docteur.rb
+++ app/models/docteur.rb
@@ -2,4 +2,6 @@ class Docteur < ApplicationRecord
 #     has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }
 #   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
     has_one_attached :image
+    validates :names, :image, :email, :telephone, :availability, :upload_cv, :upload_diploma, :field_in,  presence: true
+  validates :email, uniqueness: true
 end
